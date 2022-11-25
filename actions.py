435bed46c456c2be9b78d7bca4cebcb439616fca
rikuriku1999 <kalashnikova1120@gmail.com>
2022-11-25 08:48:01 +0000

    ok

--- actions.py
+++ actions.py
@@ -3,26 +3,22 @@ import pandas as pd
 import os
 
 
-
 ono_cancel_list = [42,51,57,59]
 riku_cancel_list = [36,40,41,54]
 
-def datasetting(files, cancel_list):
-    data_list = []
+features = ["SajoAnker","MizuKukan","MizuAction","goho","AutoHunmuLog"]
+def datasetting(files,cancel_list,name):
     joken_list = []
     for file in files:
         path, ext = os.path.splitext(file)
         file_name = path.split('/')[1]
-
-        if not int(file_name) in cancel_list:
-            action_time_list = []
+        if int(file_name) in cancel_list:
             df = pd.read_csv(file,names=("time", "mousePos.x", "mousePos.Y", 
                                         "condition", "joken", "SajoAction",
                                         "SajoAnker", "MizuKukan", "MizuAction",
                                         "x", "goho", "autoactioncount", "Mizuhunmucount",
                                         "AutoHunmuLog", "Input.GetMouseButton(0)",
                                         "Input.GetKeyDown(KeyCode.Return)"))
-            print(file)
             num = df[df['joken'] > 1].head(1).index.values[0]
             start_num = df["time"].iat[num]
             joken = df["joken"].iat[num]
@@ -30,21 +26,24 @@ def datasetting(files, cancel_list):
             df = df[df['joken'] > 1]
             df["time"] = df["time"] - start_num
             df = df.reset_index(drop=True)
-            action_count_list = df[df["autoactioncount"].diff() > 0].index.values
-            for action_count in action_count_list:
-                action_time_list.append(df["time"].iat[action_count])
-            data_list.append(action_time_list)  
-               
-    final_df = pd.DataFrame(data_list).T
-    final_df = final_df.set_axis(joken_list, axis = "columns")
-    return final_df
+            dff_list = []
+            df = df[["time","SajoAnker","MizuKukan","MizuAction","goho","AutoHunmuLog"]]
+            for feature in features:
+                changed_values = df[df[feature].diff() > 0].index.values
+                for value in changed_values:
+                    if not value in dff_list:
+                        dff_list.append(value)
+            dff_list.sort()
+            final_df = df.iloc[dff_list]
+            toCsv(final_df,joken,name)
 
-name_dict = {"riku":riku_cancel_list, "ono":ono_cancel_list}
+def toCsv(df,joken,name):
+    df.to_csv("output/" + name + str(joken) + '.csv')
 
 
+name_dict = {"stephanie":[i for i in range(100)], "riku":riku_cancel_list, "ono":ono_cancel_list}
 
 if __name__ == '__main__':
     for name in name_dict.keys():
         files = glob.glob(name +'_csv/*')
-        final_df = datasetting(files, name_dict[name])
-        final_df.to_csv("output/" + name + '_recomendation.csv')
+        datasetting(files, name_dict[name], name)
